713bca2294211c6c92bdc9b3fd4c07da3514a586
Sasha T Heyburgh <sasha.heyburgh@gmail.com>
2026-03-18 14:27:47 +0200

    Assigned UI elements to variables

--- app/src/main/java/com/example/inputoutputact/MainActivity.kt
+++ app/src/main/java/com/example/inputoutputact/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.inputoutputact
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //assigning UI elements to variables
+        val nameTxt = findViewById<EditText>(R.id.nameTxt)
+        val zuluSwitch = findViewById<Switch>(R.id.zuluSwitch)
+        val displayTxt = findViewById<TextView>(R.id.displayTxt)
+        val clickMeBtn = findViewById<Button>(R.id.clickMeBtn)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
